b4ea3ee4310b790a4d699ed5fcff3786b1b77aef
ARNAB BASAK <basakarnab912@gmail.com>
2024-03-06 22:00:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import flask
 from flask import Flask, request, jsonify
 import requests
 import random
-import streamlit as st
+# import streamlit as st
 app = Flask(__name__)
 
-st.header("App is working..")
+# st.header("App is working..")
 @app.route('/')
 def home():
     return 'Hello, World!'
